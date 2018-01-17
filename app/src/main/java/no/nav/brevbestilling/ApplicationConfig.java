e905ceadf5782edc1997d0b20321905ff641ae7b
F151389 <ketill.fenne@nav.no>
2018-01-17 12:31:10 +0100

    Lagt til prometheus

--- app/src/main/java/no/nav/brevbestilling/ApplicationConfig.java
+++ app/src/main/java/no/nav/brevbestilling/ApplicationConfig.java
@@ -1,10 +1,16 @@
 package no.nav.brevbestilling;
 
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
+import io.prometheus.client.spring.web.EnablePrometheusTiming;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@EnablePrometheusEndpoint
+@EnablePrometheusTiming
+@EnableSpringBootMetricsCollector
 @Configuration
 public class ApplicationConfig {
 	@Bean
