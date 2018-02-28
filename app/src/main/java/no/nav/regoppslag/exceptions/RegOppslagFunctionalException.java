cf7d0a84b59de265ffdd983f60dfffbaa25b80b0
Jarl Øystein Samseth <jarl.oystein.samseth@nav.no>
2018-02-28 19:55:12 +0100

    PKANAKONDA-4852: korrigert httpstatus for REST-exceptions

--- app/src/main/java/no/nav/regoppslag/exceptions/RegOppslagFunctionalException.java
+++ app/src/main/java/no/nav/regoppslag/exceptions/RegOppslagFunctionalException.java
@@ -1,9 +1,12 @@
 package no.nav.regoppslag.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * @author Jarl Øystein Samseth, Visma Consulting
  */
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class RegOppslagFunctionalException extends Exception {
 	public RegOppslagFunctionalException() {
 	}
--- app/src/main/java/no/nav/regoppslag/exceptions/RegOppslagTechnicalException.java
+++ app/src/main/java/no/nav/regoppslag/exceptions/RegOppslagTechnicalException.java
@@ -1,12 +1,9 @@
 package no.nav.regoppslag.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 /**
  * @author Jarl Øystein Samseth, Visma Consulting
  */
-@ResponseStatus(value = HttpStatus.FAILED_DEPENDENCY)
+
 public class RegOppslagTechnicalException extends Exception {
 	public RegOppslagTechnicalException() {
 	}
