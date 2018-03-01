d5e12cdacf027d16a4dd925f1cd8e5ce3b7ec2e1
Jarl Øystein Samseth <jarl.oystein.samseth@nav.no>
2018-03-01 18:27:18 +0100

    PKANAKONDA-4852: LDAP Authentication og caching lagt til i applikasjonens sikkerhet.

---
+++ app/src/main/java/no/nav/regoppslag/config/fasit/LdapAlias.java
@@ -0,0 +1,45 @@
+package no.nav.regoppslag.config.fasit;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.Valid;
+
+/**
+ * @author Joakim Bjørnstad, Jbit AS
+ */
+@Getter
+@Setter
+@ToString
+@Validated
+@Configuration
+@ConfigurationProperties("security")
+public class LdapAlias {
+
+	@NotEmpty
+	private String url;
+	@NotEmpty
+	private String basedn;
+	@NotEmpty
+	private String username;
+	@NotEmpty
+	private String password;
+	@NotEmpty
+	private String requiredroledn;
+
+	@Valid
+	private final Serviceuser serviceuser = new Serviceuser();
+
+	@Getter
+	@Setter
+	@ToString
+	public static class Serviceuser {
+		@NotEmpty
+		private String basedn;
+	}
+}
---
+++ app/src/main/java/no/nav/regoppslag/config/security/provider/rest/CachingLdapAuthenticationProvider.java
@@ -0,0 +1,51 @@
+package no.nav.regoppslag.config.security.provider.rest;
+
+import static no.nav.regoppslag.config.security.provider.rest.SecurityConfig.LDAP_CACHE;
+import static org.springframework.security.core.authority.AuthorityUtils.NO_AUTHORITIES;
+
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.ldap.authentication.LdapAuthenticationProvider;
+import org.springframework.security.ldap.authentication.LdapAuthenticator;
+import org.springframework.security.ldap.userdetails.LdapAuthoritiesPopulator;
+
+/**
+ * @author Hans Petter Simonsen - Miles
+ */
+public class CachingLdapAuthenticationProvider extends LdapAuthenticationProvider {
+
+	private final CacheManager cacheManager;
+
+	CachingLdapAuthenticationProvider(LdapAuthenticator authenticator, LdapAuthoritiesPopulator authoritiesPopulator, CacheManager cacheManager) {
+		super(authenticator, authoritiesPopulator);
+		this.cacheManager = cacheManager;
+	}
+
+	@Override
+	public Authentication authenticate(Authentication authentication)
+			throws AuthenticationException {
+		Cache ldapCache = cacheManager.getCache(LDAP_CACHE);
+		String userName = authentication.getName();
+		Integer cachedAuthHash = ldapCache.get(userName, Integer.class);
+		if(cachedAuthHash == null) {
+			Authentication authSuccess = super.authenticate(authentication);
+			ldapCache.put(userName, authentication.hashCode());
+			return authSuccess;
+		} else {
+			return cachedAuthenticate(cachedAuthHash, authentication);
+		}
+	}
+
+	private Authentication cachedAuthenticate(Integer cachedAuthHash, Authentication requestAuth) {
+		if(cachedAuthHash == requestAuth.hashCode()) {
+			return new UsernamePasswordAuthenticationToken(requestAuth.getPrincipal(), requestAuth.getCredentials(), NO_AUTHORITIES);
+		} else {
+			throw new BadCredentialsException(this.messages.getMessage(
+					"LdapAuthenticationProvider.badCredentials", "Bad credentials"));
+		}
+	}
+}
