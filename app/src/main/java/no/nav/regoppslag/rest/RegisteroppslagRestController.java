e863f894b96762011888123dac595b39207db234
Ugur Alpay Cenar <ugur.alpay.cenar@nav.no>
2018-03-09 16:06:46 +0100

    PKANAKONDA-4852 Rydding og forsøk på å få kjørt appen gjennom pipeline

--- app/src/main/java/no/nav/regoppslag/rest/RegisteroppslagRestController.java
+++ app/src/main/java/no/nav/regoppslag/rest/RegisteroppslagRestController.java
@@ -1,6 +1,5 @@
 package no.nav.regoppslag.rest;
 
-import no.nav.regoppslag.config.sts.STSClientService;
 import no.nav.regoppslag.exceptions.RegOppslagFunctionalException;
 import no.nav.regoppslag.exceptions.RegOppslagTechnicalException;
 import no.nav.regoppslag.service.RegOppslagService;
@@ -10,8 +9,6 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
@@ -26,9 +23,6 @@ public class RegisteroppslagRestController {
 	private RegOppslagService regOppslagService;
 	
 	@Inject
-	private STSClientService stsClientService;
-	
-	@Inject
 	public RegisteroppslagRestController(RegOppslagService regOppslagService) {
 		this.regOppslagService = regOppslagService;
 	}
@@ -40,9 +34,4 @@ public class RegisteroppslagRestController {
 		return RegOppslagResponseTo.builder().brevdata(requestBody.getBrevdata()).build();
 	}
 	
-	
-	@RequestMapping(value = "/token", method = RequestMethod.GET)
-	public String pingTest(){
-		return stsClientService.echo();
-	}
 }
