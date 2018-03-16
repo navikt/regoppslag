72c81c41aa0229710e6c657ed36f13e97597cd9c
Ketill Fenne <ketill.fenne@nav.no>
2018-03-16 08:13:51 +0100

    PKANAKONDA-4934 Endringer i felles.xsd samt egen plugin for NavEnhet Navn

--- app/src/main/java/no/nav/regoppslag/config/OrchestratorConfig.java
+++ app/src/main/java/no/nav/regoppslag/config/OrchestratorConfig.java
@@ -2,6 +2,7 @@ package no.nav.regoppslag.config;
 
 import no.nav.regoppslag.treg001.Orchestrator;
 import no.nav.regoppslag.treg001.plugins.MottakerPlugin;
+import no.nav.regoppslag.treg001.plugins.NavOrgenhetNavnPlugin;
 import no.nav.regoppslag.treg001.plugins.NavOrgenhetPlugin;
 import no.nav.regoppslag.treg001.plugins.SaksbehandlerPlugin;
 import no.nav.regoppslag.xmlenricher.ElementEnricherPluginRegistry;
@@ -20,7 +21,10 @@ public class OrchestratorConfig {
 	public ElementEnricherPluginRegistry registry(ApplicationContext applicationContext) throws DuplicatedElementSupportException {
 		ElementEnricherPluginRegistry registry = new SimplePluginRegistry(applicationContext);
 		registry.registerPlugin(new QName("http://nav.no/dok/pesysbrev/felles/v1/PesysFelles", "mottaker"), MottakerPlugin.class);
-		registry.registerPlugin(new QName("http://nav.no/dok/pesysbrev/felles/v1/PesysFelles", "signerendeSaksbehandler"), SaksbehandlerPlugin.class);
+		registry.registerPlugin(new QName("http://nav.no/dok/pesysbrev/felles/v1/PesysFelles", "behandlendeEnhet"), NavOrgenhetNavnPlugin.class);
+		registry.registerPlugin(new QName("http://nav.no/dok/pesysbrev/felles/v1/SaksBehandler", "navAnsatt"), SaksbehandlerPlugin.class);
+		registry.registerPlugin(new QName("http://nav.no/dok/pesysbrev/felles/v1/PesysFelles", "navEnhet"), SaksbehandlerPlugin.class);
+		registry.registerPlugin(new QName("http://nav.no/dok/pesysbrev/felles/v1/PesysFelles", "signerendeBeslutter"), SaksbehandlerPlugin.class);
 		registry.registerPlugin(new QName("http://nav.no/dok/pesysbrev/felles/v1/PesysFelles", "kontaktinformasjon"), NavOrgenhetPlugin.class);
 		return registry;
 	}
