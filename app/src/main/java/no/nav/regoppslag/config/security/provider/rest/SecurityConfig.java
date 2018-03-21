e4628e730d3fdd8537b0ec01efce7e8b0a463621
Ugur Alpay Cenar <ugur.alpay.cenar@nav.no>
2018-03-21 16:56:24 +0100

    PKANAKONDA-4852 Disable sikkerhet

--- app/src/main/java/no/nav/regoppslag/config/security/provider/rest/SecurityConfig.java
+++ app/src/main/java/no/nav/regoppslag/config/security/provider/rest/SecurityConfig.java
@@ -38,11 +38,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.antMatchers("/isAlive","/isReady","/internal/**").permitAll();
-		http.authorizeRequests().antMatchers(REST+"**")
-				.fullyAuthenticated()
-				.and().httpBasic();
+//		http.authorizeRequests()
+//				.antMatchers("/isAlive","/isReady","/internal/**").permitAll();
+//		http.authorizeRequests().antMatchers(REST+"**")
+//				.fullyAuthenticated()
+//				.and().httpBasic();
 		
 		http.csrf().disable(); //Innloggingen er stateless og uten cookies, så dette er trygt.
 	}
