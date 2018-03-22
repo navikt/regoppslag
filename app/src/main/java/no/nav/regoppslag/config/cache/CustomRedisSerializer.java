f2fd588e26d43a45c767c989bc7519ed8075f279
Ugur Alpay Cenar <ugur.alpay.cenar@nav.no>
2018-03-22 08:03:33 +0100

    PKANAKONDA-4852 Lagt til egen serializer

---
+++ app/src/main/java/no/nav/regoppslag/config/cache/CustomRedisSerializer.java
@@ -0,0 +1,44 @@
+package no.nav.regoppslag.config.cache;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.ByteBufferInput;
+import com.esotericsoftware.kryo.io.ByteBufferOutput;
+import com.esotericsoftware.kryo.pool.KryoPool;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.SerializationException;
+
+/**
+ * @author Ugur Alpay Cenar, Visma Consulting.
+ */
+public class CustomRedisSerializer implements RedisSerializer<Object> {
+	
+	private KryoPool kryoPool = new KryoPool.Builder(Kryo::new).build();
+	
+	@Override
+	public byte[] serialize(Object o) throws SerializationException {
+		ByteBufferOutput output = new ByteBufferOutput();
+		Kryo kryo = kryoPool.borrow();
+		try {
+			kryo.writeClassAndObject(output, o);
+		} finally {
+			kryoPool.release(kryo);
+			output.close();
+		}
+		
+		return output.toBytes();
+	}
+	
+	@Override
+	public Object deserialize(byte[] bytes) throws SerializationException {
+		if(bytes.length == 0) return null;
+		
+		Kryo kryo = kryoPool.borrow();
+		Object o;
+		try {
+			o = kryo.readClassAndObject(new ByteBufferInput(bytes));
+		} finally {
+			kryoPool.release(kryo);
+		}
+		return o;
+	}
+}
