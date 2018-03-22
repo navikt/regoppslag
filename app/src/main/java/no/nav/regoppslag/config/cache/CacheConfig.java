5817eb83d47360d4de7bcb66824012bc9de4e6ce
Ugur Alpay Cenar <ugur.alpay.cenar@nav.no>
2018-03-22 08:26:56 +0100

    PKANAKONDA-4852 Satt load remote caches til false

--- app/src/main/java/no/nav/regoppslag/config/cache/CacheConfig.java
+++ app/src/main/java/no/nav/regoppslag/config/cache/CacheConfig.java
@@ -7,38 +7,27 @@ import static no.nav.regoppslag.consumer.norg2.OrganisasjonEnhetKontaktinformasj
 import static no.nav.regoppslag.consumer.organisasjonv4.OrganisasjonV4Consumer.HENT_ORGANISASJON;
 import static no.nav.regoppslag.consumer.personv3.PersonV3Consumer.HENT_PERSON;
 
-import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.caffeine.CaffeineCache;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisNode;
 import org.springframework.data.redis.connection.RedisSentinelConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisSentinelConnection;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
-import redis.clients.jedis.JedisPoolConfig;
-import redis.clients.jedis.JedisSentinelPool;
 
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author Jarl Øystein Samseth, Visma Consulting
  */
 @Configuration
 @EnableCaching
+@Profile("nais")
 public class CacheConfig {
 	private static final String MASTER_NAME = "mymaster";
 	private @Value("${app.name}") String APPNAME;
@@ -55,7 +44,7 @@ public class CacheConfig {
 		//default expiration in seconds (equal to two days)
 		redisCacheManager.setDefaultExpiration(daysToSeconds(2));
 		redisCacheManager.setCacheNames(Arrays.asList(HENT_FULLT_NAVN, LDAP_CACHE_RS_LOGIN, HENT_ENHET_NAVN,HENT_DOKKAT_SPRAAKINFO, HENT_PERSON, HENT_ORGANISASJON));
-		redisCacheManager.setLoadRemoteCachesOnStartup(true);
+		redisCacheManager.setLoadRemoteCachesOnStartup(false);
 		return redisCacheManager;
 	}
 	
