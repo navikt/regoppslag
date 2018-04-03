82ebd22300ed04a053b76fb0aae851621c6e4b44
Ugur Alpay Cenar <ugur.alpay.cenar@nav.no>
2018-04-03 16:57:04 +0200

    PKANAKONDA-4852 Fjernet imports

--- app/src/main/java/no/nav/regoppslag/config/cache/CacheConfig.java
+++ app/src/main/java/no/nav/regoppslag/config/cache/CacheConfig.java
@@ -5,19 +5,15 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.CacheErrorHandler;
-import org.springframework.cache.support.CompositeCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisNode;
 import org.springframework.data.redis.connection.RedisSentinelConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-
-import java.util.Arrays;
 
 /**
  * @author Jarl Øystein Samseth, Visma Consulting
