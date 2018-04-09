90b151f21e8d4d858480a684835325316dd2c76b
Ugur Alpay Cenar <ugur.alpay.cenar@nav.no>
2018-04-09 12:07:29 +0200

    PKANAKONDA-4852 Endringer i cacheConfig

--- app/src/main/java/no/nav/regoppslag/config/cache/CacheConfig.java
+++ app/src/main/java/no/nav/regoppslag/config/cache/CacheConfig.java
@@ -1,12 +1,10 @@
 package no.nav.regoppslag.config.cache;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.CacheErrorHandler;
-import org.springframework.cache.support.NoOpCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -23,7 +21,6 @@ import org.springframework.data.redis.core.RedisTemplate;
 @Profile("nais")
 @Configuration
 @EnableCaching
-@Slf4j
 public class CacheConfig extends CachingConfigurerSupport {
 	
 	private static final String MASTER_NAME = "mymaster";
@@ -34,26 +31,19 @@ public class CacheConfig extends CachingConfigurerSupport {
 	private final CustomRedisSerializer customRedisSerializer = new CustomRedisSerializer();
 	
 	@Bean
-	@Override
-	public CacheManager cacheManager() {
-		try {
-			RedisCacheManager redisCacheManager = new RedisCacheManager(redisTemplate());
-			
-			//default expiration in seconds (equal to two days)
-			redisCacheManager.setDefaultExpiration(daysToSeconds(2));
-			redisCacheManager.setLoadRemoteCachesOnStartup(true);
-			return redisCacheManager;
-		} catch (Exception e){
-			log.warn(e.getMessage());
-			return new NoOpCacheManager();
-		}
-		
+	public CacheManager cacheManager(RedisTemplate redisTemplate) {
+		RedisCacheManager redisCacheManager = new RedisCacheManager(redisTemplate);
+		//default expiration in seconds (equal to two days)
+		redisCacheManager.setDefaultExpiration(daysToSeconds(1));
+		redisCacheManager.setLoadRemoteCachesOnStartup(true);
+		return redisCacheManager;
 	}
 	
 	@Bean
-	public RedisTemplate<?, ?> redisTemplate() {
+	public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		
 		RedisTemplate<?, ?> redisTemplate = new RedisTemplate();
-		redisTemplate.setConnectionFactory(redisConnectionFactory());
+		redisTemplate.setConnectionFactory(redisConnectionFactory);
 		
 		redisTemplate.setDefaultSerializer(customRedisSerializer);
 		redisTemplate.setEnableDefaultSerializer(true);
@@ -62,25 +52,22 @@ public class CacheConfig extends CachingConfigurerSupport {
 	}
 	
 	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		
+		JedisConnectionFactory factory = new JedisConnectionFactory(new RedisSentinelConfiguration()
+				.master(MASTER_NAME).sentinel(new RedisNode("rfs-" + APPNAME, 26379)));
+		factory.setUsePool(false); //Fører til at det blir kastet exception
+		//Timeout i ms
+		factory.setTimeout(2000);
+		return factory;
+	}
+	
+	@Bean
 	@Override
 	public CacheErrorHandler errorHandler(){
 		return new CustomCacheErrorHandler();
 	}
 	
-	@Bean
-	public RedisConnectionFactory redisConnectionFactory() {
-		
-		try {
-			JedisConnectionFactory factory = new JedisConnectionFactory(new RedisSentinelConfiguration()
-					.master(MASTER_NAME).sentinel(new RedisNode("rfs-" + APPNAME, 26379)));
-			factory.setUsePool(true);
-			return factory;
-		} catch (Exception e){
-			log.warn("Not connected to redis cache");
-			return null;
-		}
-	
-	}
 	
 	private Long daysToSeconds(Integer days) {
 		return days * 24L * 60L * 60L;
