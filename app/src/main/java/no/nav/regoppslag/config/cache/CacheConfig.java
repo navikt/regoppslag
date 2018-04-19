942a0a5b83d22a93401529929d2409db794ab3ea
Ugur Alpay Cenar <ugur.alpay.cenar@nav.no>
2018-04-19 14:34:29 +0200

    PKANAKONDA-4852 Timeout endring

--- app/src/main/java/no/nav/regoppslag/config/cache/CacheConfig.java
+++ app/src/main/java/no/nav/regoppslag/config/cache/CacheConfig.java
@@ -79,10 +79,10 @@ public class CacheConfig extends CachingConfigurerSupport {
 		CustomLettucePool lettucePool = new CustomLettucePool(new RedisSentinelConfiguration()
 				.master(MASTER_NAME).sentinel(new RedisNode("rfs-" + appName, 26379)));
 		lettucePool.setClientResources(DefaultClientResources.builder()
-				.reconnectDelay(Delay.constant(1, TimeUnit.MILLISECONDS))
+				.reconnectDelay(Delay.constant(100, TimeUnit.MILLISECONDS))
 				.build());
 		lettucePool.setPoolConfig(poolConfig());
-		lettucePool.setTimeout(200);
+		lettucePool.setTimeout(100);
 		lettucePool.afterPropertiesSet();
 		return lettucePool;
 	}
