a953c5d09d6e3ec0bdba2bfc4b3940fa7e093b5c
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2018-08-02 15:55:39 +0200

    MMA-295 Javadoc

--- app/src/main/java/no/nav/regoppslag/xmlenricher/util/AttributeValueNamespaceResolver.java
+++ app/src/main/java/no/nav/regoppslag/xmlenricher/util/AttributeValueNamespaceResolver.java
@@ -9,7 +9,17 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
 /**
- *  //TODO: VIKTIG! Legg inn doc her om hva denne klassen gjør
+ *  I tilfeller der attributtverdier i xml-elementer er kvalifiserte, f.eks {@code xsi:type="nav:Person"}, er det ikke alltid
+ *  nødvendige namespace-deklarasjoner ligger med på element-nivå. Dette fungerer oftest greit når man marshaller hele xml dokumentet,
+ *  fordi namespace-deklarasjonene ofte ligger på toppen på rotelementet og arves av underelementene. Når man skal marshalle frittstående
+ *  enkeltelementer som er løskoblet fra rot-elementet, må alle nødvendige namespace-deklarasjoner ligge med.
+ *  Denne klassen løser et subsett av disse tilfellene ved å legge på namespace-deklarasjoner som mangler,
+ *  f.eks {@code xmlns:nav="http://nav.no/dok/brevdata/felles/v1/NAVFelles"}.
+ *  Denne klassen leter etter attributter med localname 'type' på elementnoden.
+ *  * den går _ikke_ rekursivt gjennom underelementer
+ *  * den behandler _ikke_ andre kvalifiserte attributtverdier enn for attributtet xsi:type
+ *  Dersom namespacet ikke allerede er deklarert på elementet søker vi gjennom elementet, dernest gjennom xml dokumentet for å
+ *  finne tilhørende namespace for prefixet og legger dette til på elementet.
  *
  * @author Ugur Alpay Cenar, Visma Consulting.
  */
@@ -17,6 +27,15 @@ import org.w3c.dom.Node;
 public class AttributeValueNamespaceResolver {
 	private static final String NODE_NAME_TYPE="type";
 
+	/**
+	 *  Denne metoden leter etter attributter med localname 'type' på elementnoden {@code xpathResult}.
+	 *  * den går _ikke_ rekursivt gjennom underelementer
+	 *  * den behandler _ikke_ andre kvalifiserte attributtverdier enn for attributtet {@code xsi:type}
+	 *  Dersom namespacet ikke allerede er deklarert på elementet søker vi gjennom elementet {@code xpathResult}, dernest
+	 *  gjennom xml dokumentet {@code xmlDocument} for å finne tilhørende namespace for prefixet og legger dette til på elementet.
+	 * @param xmlDocument Hele xml dokumentet
+	 * @param xpathResult En enkel xml node fra xml dokumentet
+	 */
 	public void resolveNamespace(Document xmlDocument, Node xpathResult){
 
 		// Case of qualified attribute values, we're forced to add corresponding namespace declaration manually
