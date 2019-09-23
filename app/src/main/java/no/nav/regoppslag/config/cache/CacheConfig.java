531427c400a9c91bad0084ab4b12309bd91bced5
Martin Burheim Tingstad <Martin.Burheim.Tingstad@nav.no>
2019-09-23 17:07:16 +0200

    MMA-3358: Tatt tilbake kryo custom serializer

--- app/src/main/java/no/nav/regoppslag/config/cache/CacheConfig.java
+++ app/src/main/java/no/nav/regoppslag/config/cache/CacheConfig.java
@@ -61,30 +61,25 @@ public class CacheConfig extends CachingConfigurerSupport {
 	public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
 		//Remaining caches uses the default value
 		HashMap<String, RedisCacheConfiguration> initialConfigs = new HashMap<>();
-		initialConfigs.put(STS_CACHE_NAME, generateCacheConfig(STS_CACHE_EXPIRATION_TIME, null));
-		initialConfigs.put(HENT_PERSON, generateCacheConfig(HENT_PERSON_CACHE_EXPIRATION_TIME, null));
-		initialConfigs.put(HENT_DOKKAT_SPRAAKINFO, generateCacheConfig(DEFAULT_CACHE_EXPIRATION_TIME, null));
-		initialConfigs.put(HENT_ENHET_NAVN, generateCacheConfig(DEFAULT_CACHE_EXPIRATION_TIME, Organisasjonsenhet.class));
-		initialConfigs.put(HENT_ORGANISASJON, generateCacheConfig(DEFAULT_CACHE_EXPIRATION_TIME, null));
+		initialConfigs.put(STS_CACHE_NAME, generateCacheConfig(STS_CACHE_EXPIRATION_TIME));
+		initialConfigs.put(HENT_PERSON, generateCacheConfig(HENT_PERSON_CACHE_EXPIRATION_TIME));
+		//initialConfigs.put(HENT_DOKKAT_SPRAAKINFO, generateCacheConfig(DEFAULT_CACHE_EXPIRATION_TIME));
+		//initialConfigs.put(HENT_ENHET_NAVN, generateCacheConfig(DEFAULT_CACHE_EXPIRATION_TIME));
+		//initialConfigs.put(HENT_ORGANISASJON, generateCacheConfig(DEFAULT_CACHE_EXPIRATION_TIME));
 
 		return RedisCacheManager.builder(connectionFactory)
-				//.cacheDefaults(generateCacheConfig(DEFAULT_CACHE_EXPIRATION_TIME, null))
+				.cacheDefaults(generateCacheConfig(DEFAULT_CACHE_EXPIRATION_TIME))
 				.withInitialCacheConfigurations(initialConfigs)
 				.build();
 	}
 
-	private RedisCacheConfiguration generateCacheConfig(Duration duration, Class cachedClass) {
+	private RedisCacheConfiguration generateCacheConfig(Duration duration) {
 		RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig();
 		if(duration != null) {
 			redisCacheConfiguration.entryTtl(duration);
 		}
 		redisCacheConfiguration.serializeKeysWith(fromSerializer(new StringRedisSerializer()));
-		if(cachedClass != null) {
-			redisCacheConfiguration.serializeValuesWith(fromSerializer(new Jackson2JsonRedisSerializer<Object>(cachedClass)));
-		}
-		else {
-			redisCacheConfiguration.serializeValuesWith(fromSerializer(new StringRedisSerializer()));
-		}
+		redisCacheConfiguration.serializeValuesWith(fromSerializer(new CustomRedisSerializer()));
 		return redisCacheConfiguration;
 	}
 
---
+++ app/src/main/java/no/nav/regoppslag/config/cache/CustomRedisSerializer.java
@@ -0,0 +1,50 @@
+package no.nav.regoppslag.config.cache;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.ByteBufferInput;
+import com.esotericsoftware.kryo.io.ByteBufferOutput;
+import com.esotericsoftware.kryo.pool.KryoPool;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+/**
+ * @author Ugur Alpay Cenar, Visma Consulting.
+ */
+public class CustomRedisSerializer implements RedisSerializer<Object> {
+	
+	private final KryoPool kryoPool;
+	
+	private static final Integer MIN_BUFFER_SIZE=1024;
+
+	public CustomRedisSerializer() {
+		this.kryoPool = new KryoPool.Builder(Kryo::new).build();
+	}
+	
+	@Override
+	public byte[] serialize(Object o)  {
+		ByteBufferOutput output = new ByteBufferOutput(MIN_BUFFER_SIZE, -1); //-1 means maximum possible buffer size on VM.
+		Kryo kryo = kryoPool.borrow();
+		try {
+			kryo.writeClassAndObject(output, o);
+		} finally {
+			kryoPool.release(kryo);
+			output.close();
+		}
+
+		return output.toBytes();
+	}
+	
+	@Override
+	public Object deserialize(byte[] bytes) {
+		if(bytes.length == 0) {
+			return null;
+		}
+		Kryo kryo = kryoPool.borrow();
+		Object o;
+		try {
+			o = kryo.readClassAndObject(new ByteBufferInput(bytes));
+		} finally {
+			kryoPool.release(kryo);
+		}
+		return o;
+	}
+}
