e1c301e7c5d3ecf823af7ca56f0cbfbbf38e9209
Martin Burheim Tingstad <Martin.Burheim.Tingstad@nav.no>
2019-09-23 11:25:37 +0200

    MMA-3358: Endret til JSON-serialisering

--- app/src/main/java/no/nav/regoppslag/config/cache/CacheConfig.java
+++ app/src/main/java/no/nav/regoppslag/config/cache/CacheConfig.java
@@ -1,11 +1,17 @@
 package no.nav.regoppslag.config.cache;
 
+import static no.nav.regoppslag.consumer.dokkat.Tkat020DokumenttypeInfo.HENT_DOKKAT_SPRAAKINFO;
+import static no.nav.regoppslag.consumer.norg2.OrganisasjonEnhetKontaktinformasjonV1Consumer.HENT_ENHET_NAVN;
 import static no.nav.regoppslag.consumer.personv3.PersonV3Consumer.HENT_PERSON;
 import static no.nav.regoppslag.nais.NaisCheckSTSTokenRetriever.STS_CACHE_NAME;
+import static org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair.fromSerializer;
 
 import io.lettuce.core.ClientOptions;
 import io.lettuce.core.SocketOptions;
 import lombok.extern.slf4j.Slf4j;
+import no.nav.dokkat.api.tkat020.v3.SpraakInfoTo;
+import no.nav.tjeneste.virksomhet.organisasjonenhetkontaktinformasjon.v1.informasjon.Organisasjonsenhet;
+import no.nav.tjeneste.virksomhet.person.v3.informasjon.Bruker;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
@@ -22,6 +28,8 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
 import java.util.HashMap;
@@ -37,9 +45,9 @@ import java.util.HashMap;
 @Slf4j
 public class CacheConfig extends CachingConfigurerSupport {
 	
-	static final Duration DEFAULT_CACHE_EXPIRATION_TIME = Duration.ofSeconds(2);
+	static final Duration DEFAULT_CACHE_EXPIRATION_TIME = Duration.ofSeconds(2L);
 	static final Duration HENT_PERSON_CACHE_EXPIRATION_TIME = Duration.ofSeconds(10L);
-	static final Duration STS_CACHE_EXPIRATION_TIME = Duration.ofSeconds(50);
+	static final Duration STS_CACHE_EXPIRATION_TIME = Duration.ofSeconds(50L);
 
 	@Value("${redis.hostname:regoppslag-redis}")
 	private String redisHost;
@@ -51,18 +59,27 @@ public class CacheConfig extends CachingConfigurerSupport {
 	public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
 		//Remaining caches uses the default value
 		HashMap<String, RedisCacheConfiguration> initialConfigs = new HashMap<>();
-		initialConfigs.put(STS_CACHE_NAME, generateConfigWithDuration(STS_CACHE_EXPIRATION_TIME));
-		initialConfigs.put(HENT_PERSON, generateConfigWithDuration(HENT_PERSON_CACHE_EXPIRATION_TIME));
+		initialConfigs.put(STS_CACHE_NAME, generateCacheConfig(STS_CACHE_EXPIRATION_TIME, null));
+		initialConfigs.put(HENT_PERSON, generateCacheConfig(HENT_PERSON_CACHE_EXPIRATION_TIME, Bruker.class));
+		initialConfigs.put(HENT_DOKKAT_SPRAAKINFO, generateCacheConfig(DEFAULT_CACHE_EXPIRATION_TIME, SpraakInfoTo.class));
+		initialConfigs.put(HENT_ENHET_NAVN, generateCacheConfig(DEFAULT_CACHE_EXPIRATION_TIME, Organisasjonsenhet.class));
 
 		return RedisCacheManager.builder(connectionFactory)
-				.cacheDefaults(generateConfigWithDuration(DEFAULT_CACHE_EXPIRATION_TIME))
+				.cacheDefaults(generateCacheConfig(DEFAULT_CACHE_EXPIRATION_TIME, null))
 				.withInitialCacheConfigurations(initialConfigs)
 				.build();
 	}
 
-	private RedisCacheConfiguration generateConfigWithDuration(Duration duration) {
-		return RedisCacheConfiguration.defaultCacheConfig()
-				.entryTtl(duration);
+	private RedisCacheConfiguration generateCacheConfig(Duration duration, Class cachedClass) {
+		RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig();
+		if(duration != null) {
+			redisCacheConfiguration.entryTtl(duration);
+		}
+		if(cachedClass != null) {
+			redisCacheConfiguration.serializeKeysWith(fromSerializer(new StringRedisSerializer()));
+			redisCacheConfiguration.serializeValuesWith(fromSerializer(new Jackson2JsonRedisSerializer<>(cachedClass)));
+		}
+		return redisCacheConfiguration;
 	}
 
 	@Bean
