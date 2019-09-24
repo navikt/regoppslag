6886be518b859dace7d7051b93dd934b9d583b25
Martin Burheim Tingstad <Martin.Burheim.Tingstad@nav.no>
2019-09-24 10:16:04 +0200

    MMA-3358: Konfigurerer hentEnhetNavn-cache eksplisitt

--- app/src/main/java/no/nav/regoppslag/config/cache/CacheConfig.java
+++ app/src/main/java/no/nav/regoppslag/config/cache/CacheConfig.java
@@ -63,6 +63,7 @@ public class CacheConfig extends CachingConfigurerSupport {
 		HashMap<String, RedisCacheConfiguration> initialConfigs = new HashMap<>();
 		initialConfigs.put(STS_CACHE_NAME, generateCacheConfig(STS_CACHE_EXPIRATION_TIME));
 		initialConfigs.put(HENT_PERSON, generateCacheConfig(HENT_PERSON_CACHE_EXPIRATION_TIME));
+		initialConfigs.put(HENT_ENHET_NAVN, generateCacheConfig(DEFAULT_CACHE_EXPIRATION_TIME));
 
 		return RedisCacheManager.builder(connectionFactory)
 				.cacheDefaults(generateCacheConfig(DEFAULT_CACHE_EXPIRATION_TIME))
@@ -75,6 +76,7 @@ public class CacheConfig extends CachingConfigurerSupport {
 		if(duration != null) {
 			redisCacheConfiguration.entryTtl(duration);
 		}
+
 		redisCacheConfiguration.serializeKeysWith(fromSerializer(new CustomRedisSerializer<>()));
 		redisCacheConfiguration.serializeValuesWith(fromSerializer(new CustomRedisSerializer<>()));
 		return redisCacheConfiguration;
