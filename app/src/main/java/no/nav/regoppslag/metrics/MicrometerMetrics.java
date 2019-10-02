c76771976d65551ba64cee46a275cf12f77d5c90
Martin Burheim Tingstad <Martin.Burheim.Tingstad@nav.no>
2019-10-02 13:41:38 +0200

    MMA-3358: Fjerner unused imports

--- app/src/main/java/no/nav/regoppslag/metrics/MicrometerMetrics.java
+++ app/src/main/java/no/nav/regoppslag/metrics/MicrometerMetrics.java
@@ -19,7 +19,6 @@ import io.micrometer.core.instrument.Meter;
 import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
