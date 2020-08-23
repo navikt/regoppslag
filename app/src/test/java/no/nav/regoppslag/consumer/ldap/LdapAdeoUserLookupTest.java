403e843b147bb073b422ebc9ba4e3b12365ab15e
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2020-08-23 17:31:02 +0200

    MMA-4742 Fjern ldap test brukernavn og passord.

--- app/src/test/java/no/nav/regoppslag/consumer/ldap/LdapAdeoUserLookupTest.java
+++ app/src/test/java/no/nav/regoppslag/consumer/ldap/LdapAdeoUserLookupTest.java
@@ -1,8 +1,5 @@
 package no.nav.regoppslag.consumer.ldap;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import no.nav.regoppslag.config.ldap.LdapConfig;
@@ -14,11 +11,21 @@ import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.ldap.core.AttributesMapper;
+import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.ldap.query.LdapQuery;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.inject.Inject;
+import java.util.Collections;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Joakim Bjørnstad, Jbit AS
@@ -28,6 +35,8 @@ import javax.inject.Inject;
 @TestPropertySource("classpath:ldap.properties")
 public class LdapAdeoUserLookupTest {
 
+	private static final LdapTemplate ldapTemplateMock = mock(LdapTemplate.class);
+
 	@Rule
 	public ExpectedException thrown = ExpectedException.none();
 
@@ -36,15 +45,17 @@ public class LdapAdeoUserLookupTest {
 
 	@Test
 	public void shouldHentFulltNavn() throws Exception {
+		when(ldapTemplateMock.search(any(LdapQuery.class), any(AttributesMapper.class))).thenReturn(Collections.singletonList("Itest Itestesen"));
 		String fulltNavn = ldapAdeoUserLookup.hentFulltNavn("Z999990");
-		assertThat(fulltNavn, is("REQ000000342741 testbrukere for kodeverksklienten"));
+		assertThat(fulltNavn, is("Itest Itestesen"));
 	}
 
 	@Test
 	public void shouldThrowExceptionWhenAdeoIdentNotFound() throws Exception {
+		when(ldapTemplateMock.search(any(LdapQuery.class), any(AttributesMapper.class))).thenReturn(Collections.emptyList());
 		thrown.expect(RegOppslagFunctionalException.class);
 		thrown.expectMessage("Ldap.hentFulltNavn finner ikke bruker med ident=bxxxxxx");
-		String fulltNavn = ldapAdeoUserLookup.hentFulltNavn("bxxxxxx");
+		ldapAdeoUserLookup.hentFulltNavn("bxxxxxx");
 	}
 
 	static class Config {
@@ -61,6 +72,11 @@ public class LdapAdeoUserLookupTest {
 		@Bean
 		public MicrometerMetrics metrics() {
 			return new MicrometerMetrics();
+		}
+
+		@Bean
+		public LdapTemplate ldapTemplate() {
+			return ldapTemplateMock;
 		}
 
 	}
