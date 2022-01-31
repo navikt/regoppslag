03f4271ec8278a61cede8fb3d1589a6271c6b3ff
Johan Aanesen <johan.aanesen@gmail.com>
2022-01-31 11:02:35 +0100

    Update app/src/main/java/no/nav/regoppslag/rest/PostAdresseController.java
    Co-authored-by: Joakim Bjørnstad <joakim.bjornstad@nav.no>

--- app/src/main/java/no/nav/regoppslag/rest/PostAdresseController.java
+++ app/src/main/java/no/nav/regoppslag/rest/PostAdresseController.java
@@ -49,7 +49,7 @@ public class PostAdresseController {
 	@ApiResponses(value = {
 			@ApiResponse(responseCode = "200", description = "OK"),
 			@ApiResponse(responseCode = "400", description = "Ugyldig input. Denne feilen vil returneres hvis det feil i input verdiene."),
-			@ApiResponse(responseCode = "401", description = "Ingen tilgang til PersonV3"),
+			@ApiResponse(responseCode = "401", description = "Ingen tilgang til postadresse tjenesten."),
 			@ApiResponse(responseCode = "404", description = "Bruker har ukjent adresse"),
 			@ApiResponse(responseCode = "410", description = "Person er død og har ukjent adresse"),
 			@ApiResponse(responseCode = "500", description = "Teknisk feil")
