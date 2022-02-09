959ac487a782ec00aa6963ba9b773adb4b03fc08
Johan Aanesen <johan.aanesen@nav.no>
2022-02-09 13:05:06 +0100

    removing saml layer, one test still behaves irregular

--- core/src/main/java/no/nav/regoppslag/config/security/SecurityConfig.java
+++ core/src/main/java/no/nav/regoppslag/config/security/SecurityConfig.java
@@ -1,10 +1,8 @@
 package no.nav.regoppslag.config.security;
 
-import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 /**
  * Spring boot security config for Basic Authentication and authorisation with LDAP users as authenthic users.
@@ -12,12 +10,10 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
  * @author Jarl Øystein Samseth, Visma Consulting
  */
 @EnableWebSecurity
-@EnableJwtTokenValidation(ignore = {"org.springframework", "org.springdoc"})
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.addFilterBefore(new SamlTokenAuthenticationFilter(), BasicAuthenticationFilter.class);
 		http.csrf().disable();
 	}
 }
