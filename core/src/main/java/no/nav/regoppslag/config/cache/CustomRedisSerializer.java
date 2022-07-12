77a256afb6eb5f75cd200423d7f4e6ee702a5126
S161464 <hakon.snotun@nav.no>
2022-07-12 09:50:23 +0200

    MMA-5872: Kryo5

--- core/src/main/java/no/nav/regoppslag/config/cache/CustomRedisSerializer.java
+++ core/src/main/java/no/nav/regoppslag/config/cache/CustomRedisSerializer.java
@@ -3,7 +3,7 @@ package no.nav.regoppslag.config.cache;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.ByteBufferInput;
 import com.esotericsoftware.kryo.io.ByteBufferOutput;
-import com.esotericsoftware.kryo.pool.KryoPool;
+import com.esotericsoftware.kryo.util.Pool;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
 /**
@@ -11,22 +11,28 @@ import org.springframework.data.redis.serializer.RedisSerializer;
  */
 public class CustomRedisSerializer<T> implements RedisSerializer<T> {
 
-	private final KryoPool kryoPool;
+	private final Pool<Kryo> kryoPool;
 
 	private static final Integer MIN_BUFFER_SIZE=1024;
 
 	public CustomRedisSerializer() {
-		this.kryoPool = new KryoPool.Builder(Kryo::new).build();
+		this.kryoPool = new Pool<>(true, false, 16) {
+			protected Kryo create() {
+				Kryo kryo = new Kryo();
+				// Configure the Kryo instance.
+				return kryo;
+			}
+		};
 	}
 	
 	@Override
 	public byte[] serialize(T o)  {
 		ByteBufferOutput output = new ByteBufferOutput(MIN_BUFFER_SIZE, -1); //-1 means maximum possible buffer size on VM.
-		Kryo kryo = kryoPool.borrow();
+		Kryo kryo = kryoPool.obtain();
 		try {
 			kryo.writeClassAndObject(output, o);
 		} finally {
-			kryoPool.release(kryo);
+			kryoPool.free(kryo);
 			output.close();
 		}
 
@@ -38,12 +44,12 @@ public class CustomRedisSerializer<T> implements RedisSerializer<T> {
 		if(bytes.length == 0) {
 			return null;
 		}
-		Kryo kryo = kryoPool.borrow();
+		Kryo kryo = kryoPool.obtain();
 		T o;
 		try {
 			o = (T)kryo.readClassAndObject(new ByteBufferInput(bytes));
 		} finally {
-			kryoPool.release(kryo);
+			kryoPool.free(kryo);
 		}
 		return o;
 	}
