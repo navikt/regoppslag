51795c506dd233813805ab883cbe8f33d82b7ba4
tsigab <tsigab.angosom@gmail.com>
2023-03-20 09:19:31 +0100

    MMA-6692 la til constructor annotation

--- core/src/main/java/no/nav/regoppslag/consumer/norg2/to/EnhetNavn.java
+++ core/src/main/java/no/nav/regoppslag/consumer/norg2/to/EnhetNavn.java
@@ -1,12 +1,14 @@
 package no.nav.regoppslag.consumer.norg2.to;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-@JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor
+@NoArgsConstructor
 public class EnhetNavn {
 	private String enhetNr;
 	private String navn;
