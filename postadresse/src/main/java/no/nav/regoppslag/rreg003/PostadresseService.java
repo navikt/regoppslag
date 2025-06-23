1cdb2fafd0f38f8c61f32c8f40e6a5940257fa53
Kristian Laskemoen <kristian.laskemoen@nav.no>
2025-06-23 15:00:19 +0200

    legge til mellomrom slik at loggingen blir oversiktlig

--- postadresse/src/main/java/no/nav/regoppslag/rreg003/PostadresseService.java
+++ postadresse/src/main/java/no/nav/regoppslag/rreg003/PostadresseService.java
@@ -77,7 +77,7 @@ public class PostadresseService {
 
 		PdlMottakerInfo pdlMottakerInfo = mapPDLResponse.mapHentPerson(personFraPdl, SERVICE_CODE_RREG003);
 		if(KONTAKTINFORMASJONFORDØDSBO.equals(pdlMottakerInfo.getPostadresse().getAdressekilde())){
-			secureLog.info("noen har hentet KONTAKTINFORMASJONFORDØDSBO. Navn:{}, adresse{}",
+			secureLog.info("noen har hentet KONTAKTINFORMASJONFORDØDSBO. Navn: {}, adresse: {}",
 					pdlMottakerInfo.getNavn(),
 					pdlMottakerInfo.getPostadresse());
 		}
